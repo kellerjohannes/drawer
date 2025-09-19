47dc45bdba842857e9169cf7ab25dbdba503390a
Johannes Keller <johannkell@gmail.com>
2025-09-19 13:08:45 +0200

    working on interval constellations, grouping working

--- interval-progressions.lisp
+++ interval-progressions.lisp
@@ -29,14 +29,23 @@
                              (pt label-padding 0))
                          :angle 90)))))
 
-(defun make-dict-entry (id note-a note-b label)
-  (list id (vicentino-tunings:interval :tuning1 note-a :up note-b) label))
+(defun make-dict-entry (id note-a note-b label &optional (octave 0))
+  (list id
+        (* (vicentino-tunings:interval :tuning1 note-a :up note-b)
+           (expt 2 octave))
+        label))
 
 (defparameter *intervals*
-  (list (make-dict-entry :tono :d :e "tono")
-        (make-dict-entry :semitono-maggiore :e :f "semitono maggiore")
-        (make-dict-entry :terza-maggiore :f :a "terza maggiore")
-        (make-dict-entry :quinta :d :a "quinta")))
+  (list
+   (make-dict-entry :unisono :d :d "unisono" -1)
+   (make-dict-entry :semitono-maggiore :e :f "semitono maggiore")
+   (make-dict-entry :tono :d :e "tono")
+   (make-dict-entry :terza-minore :d :f "terza minore")
+   (make-dict-entry :terza-maggiore :f :a "terza maggiore")
+   (make-dict-entry :quarta :d :g "quarta")
+   (make-dict-entry :quinta :d :a "quinta")
+   (make-dict-entry :ottava :d :d "ottava")
+   (make-dict-entry :decima :c :e "decima" 1)))
 
 (defun get-interval-data (id)
   (find id *intervals* :key #'first))
@@ -47,36 +56,74 @@
 (defun get-interval-label (id)
   (third (get-interval-data id)))
 
-(defun make-2-constellation (cons1 step1 dir1 step2 dir2 cons2)
+
+(defun invert-ratio (ratio)
+  (/ 1 ratio))
+
+(defun make-2-constellation (cons1 step1 dir1 step2 dir2 cons2 &optional (v-transpose :unisono) (transpose-dir :up))
   (let ((t1 0)
         (t2 20)
-        (step-ratio1 (* (if (eq dir1 :up) 1 -1) (get-interval-ratio step1)))
-        (step-ratio2 (* (if (eq dir2 :up) 1 -1) (get-interval-ratio step2)))
+        (step-ratio1 (* (if (eq dir1 :up)
+                            (get-interval-ratio step1)
+                            (invert-ratio (get-interval-ratio step1)))))
+        (step-ratio2 (* (if (eq dir2 :up)
+                            (get-interval-ratio step2)
+                            (invert-ratio (get-interval-ratio step2)))))
         (cons-ratio1 (get-interval-ratio cons1))
         (cons-ratio2 (get-interval-ratio cons2))
         (v-factor 1/25)
         (v-padding 1.3)
         (h-padding 1.6))
-    (gr (list (make-voice-leading t1 t2 1/1 step-ratio2 (get-interval-label step2) v-factor
-                                  (- v-padding))
-              (make-voice-leading t1 t2 cons-ratio1 (* cons-ratio1 step-ratio1)
-                                  (get-interval-label step2)
-                                  v-factor
-                                  v-padding)
-              (make-consonance t1 1/1 cons-ratio1 (get-interval-label cons1) v-factor (- h-padding))
-              (make-consonance t2 step-ratio2 cons-ratio2
-                               (get-interval-label cons2) v-factor h-padding)))))
+    (cp (gr (list (make-voice-leading t1 t2 1/1 step-ratio2 (get-interval-label step2)
+                                      v-factor (- v-padding))
+                  (make-voice-leading t1 t2 cons-ratio1 step-ratio1 (get-interval-label step1)
+                                      v-factor
+                                      v-padding)
+                  (make-consonance t1 1/1 cons-ratio1 (get-interval-label cons1) v-factor (- h-padding))
+                  (make-consonance t2 step-ratio2 cons-ratio2
+                                   (get-interval-label cons2) v-factor h-padding)))
+        (pt 0 0)
+        (pt 0 (* (if (eq transpose-dir :up) 1 -1)
+                 (* v-factor (vicentino-tunings:ratio->length (get-interval-ratio v-transpose))))))))
 
-(defun doit ()
-  (let* ((btikz (make-backend-tikz :filename "tono-ascendente-1.tex"))
-         ;; (v-factor 1/25)
-         ;; (t1 0)
-         ;; (t2 20)
-         ;; (canto (make-voice-leading t1 t2 4/3 9/8 "tono ascendente" v-factor 1.3))
-         ;; (basso (make-voice-leading t1 t2 16/15 15/16 "semitono maggiore" v-factor -1.3))
-         ;; (c1 (make-consonance t1 16/15 5/4 "terza maggiore" v-factor -1.3))
-         ;; (c2 (make-consonance t2 1/1 3/2 "quinta" v-factor 1.6))
-         (c1 (make-2-constellation :terza-maggiore :tono :up :semitono-maggiore :down :quinta))
-         )
-    (draw-with-multiple-backends (list btikz) (list c1))
-    (compile-tikz btikz)))
+(defun inverse-direction (dir)
+  (if (eq dir :up) :down :up))
+
+(defun make-constellation-group (cons1 step1 dir1 step2 dir2 cons2 h-padding)
+  (let ((ca (make-2-constellation cons1 step1 dir1 step2 dir2 cons2))
+        (cb (make-2-constellation cons2 step1 (inverse-direction dir1)
+                                  step2 (inverse-direction dir2) cons1
+                                  step2 dir2))
+        (cc (make-2-constellation cons1 step2 (inverse-direction dir2) step1 (inverse-direction dir1) cons2))
+        (cd (make-2-constellation cons2 step2 dir2 step1 dir1 cons1
+                                  step1 (inverse-direction dir1))))
+    (if (eq step1 step2)
+        (gr (list ca
+                  (cp cb (pt 0 0) (pt h-padding 0))
+                  ;; (cp cc (pt 0 0) (pt (* 2 h-padding) 0))
+                  ;; (cp cd (pt 0 0) (pt (* 3 h-padding) 0))
+                  ))
+        (gr (list ca
+                  (cp cb (pt 0 0) (pt h-padding 0))
+                  (cp cc (pt 0 0) (pt (* 2 h-padding) 0))
+                  (cp cd (pt 0 0) (pt (* 3 h-padding) 0)))))))
+
+(let* ((btikz (make-backend-tikz :filename "tono-ascendente-1.tex"))
+       (h-padding 35)
+       (v-padding 65)
+       (cg1 (make-constellation-group :terza-minore :tono :up :quinta :down :ottava
+                                      h-padding))
+       (cg2 (make-constellation-group :terza-maggiore :tono :up :semitono-maggiore :down :quinta
+                                      h-padding))
+       (cg3 (make-constellation-group :terza-minore :tono :up :tono :down :quinta
+                                  h-padding))
+       (cg4 (make-constellation-group :terza-minore :tono :up :quinta :down :ottava
+                                  h-padding))
+       )
+  (draw-with-multiple-backends (list btikz) (list
+                                             cg1
+                                             (cp cg2 (pt 0 0) (pt 0 (* -1 v-padding)))
+                                             (cp cg3 (pt 0 0) (pt 0 (* -2 v-padding)))
+                                             (cp cg4 (pt 0 0) (pt 0 (* -3 v-padding)))
+                                             ))
+  (compile-tikz btikz))
